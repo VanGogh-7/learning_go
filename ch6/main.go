14b712f952335d06f2155fa7e829af4156d812ce
van-gogh <weiqifan0524@gmail.com>
2026-06-09 15:26:49 +0800

    day5.1; Jun-9.

---
+++ ch6/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+type person struct {
+	FirstName  string
+	MiddleName *string
+	LastName   string
+}
+
+func main() {
+	var x int32 = 10
+	var y bool = true
+	pointerX := &x
+	pointerY := &y
+	var pointerZ *string
+	fmt.Println(pointerX, pointerY, pointerZ)
+
+	p := person{
+		FirstName:  "John",
+		MiddleName: pointerZ,
+		LastName:   "Smith",
+	}
+	fmt.Println(p)
+}
